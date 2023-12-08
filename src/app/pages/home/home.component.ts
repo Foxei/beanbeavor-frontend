882ff1306198b6e47f7af17c1f3a6f3d145f9728
Simon Schäfer <simon.schaefer@rwth-aachen.de>
2023-12-08 19:29:51 +0100

    [SSC] Remove product list.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -22,60 +22,60 @@ export class HomeComponent {
   }
 }
 
-Euglüh
-Almdudler
-Bier (0,33L auch alk.frei–außer Leffe/Erdinger)
-Bier (0,5 L) auch alk.frei  - kein Erdinger-
-Café au lait
-Cappuccino
-Club-Mate
-Coca Cola (hi-carb)
-Coke ZERO
-Coke Zero koffeinfrei
-Chouffe Bier
-Eifler Landbier
-Eis: Cuja Mara Split
-Eis: Magnum / NUII
-Engelbert Apfel
-Engelbert Naturell
-Engelbert/Rheinfels Sprudel
-Erdinger Alkoholfrei (0,5l) 
-Erdnüsse
-Fanta
-Fassbrause/Bionade
-Gerolsteiner
-Haribo-Beutel
+// Euglüh
+// Almdudler
+// Bier (0,33L auch alk.frei–außer Leffe/Erdinger)
+// Bier (0,5 L) auch alk.frei  - kein Erdinger-
+// Café au lait
+// Cappuccino
+// Club-Mate
+// Coca Cola (hi-carb)
+// Coke ZERO
+// Coke Zero koffeinfrei
+// Chouffe Bier
+// Eifler Landbier
+// Eis: Cuja Mara Split
+// Eis: Magnum / NUII
+// Engelbert Apfel
+// Engelbert Naturell
+// Engelbert/Rheinfels Sprudel
+// Erdinger Alkoholfrei (0,5l) 
+// Erdnüsse
+// Fanta
+// Fassbrause/Bionade
+// Gerolsteiner
+// Haribo-Beutel
 
-Kaffee klein
-Kaffee groß
-Latte Macchiato
-Moccachoc
-Espresso
+// Kaffee klein
+// Kaffee groß
+// Latte Macchiato
+// Moccachoc
+// Espresso
 
-Erdinger Alkoholfrei (0.33l) 
-Knoppers Riegel
-Leffe
-Milka
-Pizza
-Rockstar/Monster (inkl. Pfand)
-Schoko-Creme (Kakao)
+// Erdinger Alkoholfrei (0.33l) 
+// Knoppers Riegel
+// Leffe
+// Milka
+// Pizza
+// Rockstar/Monster (inkl. Pfand)
+// Schoko-Creme (Kakao)
 
-Taschentücher
-Apfel
-Balisto
-Duplo
-Eis: Cornetto
-Eis: Nogger
-Oreo Kekse
-Trolli
-Twix 
-Yogurette
-Nuts Royal Nüsse
-Mandarine/Clementine
-MioMio Mate
+// Taschentücher
+// Apfel
+// Balisto
+// Duplo
+// Eis: Cornetto
+// Eis: Nogger
+// Oreo Kekse
+// Trolli
+// Twix 
+// Yogurette
+// Nuts Royal Nüsse
+// Mandarine/Clementine
+// MioMio Mate
 
-3D-Druck pro gramm
-Simplex Farbe
-Simplex S/W
-Duplex S/W
-Duplex Farbe
+// 3D-Druck pro gramm
+// Simplex Farbe
+// Simplex S/W
+// Duplex S/W
+// Duplex Farbe
